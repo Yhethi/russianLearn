c6052092cf84cfd0d8e93cf912ee3232e2260342
Yhethi <eloyjosebonillag@gmail.com>
2024-02-16 16:43:49 -0400

    Updated: title change relative to top

--- src/assets/components/Letters.jsx
+++ src/assets/components/Letters.jsx
@@ -12,26 +12,28 @@ export const Letters = () => {
   };
 
   return (
-    <div className="container__letters">
-      <div className="flex_column">
-        <div className="letters">
-          {ApiLetters.map((item, key) => {
-            return (
-              <>
-                <img
-                  className="letter"
-                  src={item.img}
-                  alt={item.img}
-                  onClick={() => onHandleMp3(item.sound, item.title)}
-                />
-              </>
-            );
-          })}
-        </div>
-        <div className="title__letter">
-          <p>{title__letter}</p>
+    <>
+      <div className="title__letter">
+        <p>{title__letter}</p>
+      </div>
+      <div className="container__letters">
+        <div className="flex_column">
+          <div className="letters">
+            {ApiLetters.map((item, key) => {
+              return (
+                <>
+                  <img
+                    className="letter"
+                    src={item.img}
+                    alt={item.img}
+                    onClick={() => onHandleMp3(item.sound, item.title)}
+                  />
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
